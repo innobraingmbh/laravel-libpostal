b9e5258c99fce252f6a8a223eda0e971d90b040b
Bruno Görß <bruno.goerss@inno-brain.de>
2025-01-23 15:04:08 +0100

    feat: add to http response

--- src/Dtos/ParsedAddress.php
+++ src/Dtos/ParsedAddress.php
@@ -4,35 +4,42 @@ declare(strict_types=1);
 
 namespace Innobrain\Libpostal\Dtos;
 
+use GuzzleHttp\Promise\PromiseInterface;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
 use Innobrain\Libpostal\Dtos\Concerns\Fromable;
 
-class ParsedAddress
+final class ParsedAddress
 {
     use Fromable;
 
     public function __construct(
-        public string $house,
-        public string $category,
-        public string $near,
-        public string $houseNumber,
-        public string $road,
-        public string $unit,
-        public string $level,
-        public string $staircase,
-        public string $entrance,
-        public string $poBox,
-        public string $postcode,
-        public string $suburb,
-        public string $cityDistrict,
-        public string $city,
-        public string $island,
-        public string $stateDistrict,
-        public string $state,
-        public string $countryRegion,
-        public string $country,
-        public string $worldRegion,
+        public string $house = '',
+        public string $category = '',
+        public string $near = '',
+        public string $houseNumber = '',
+        public string $road = '',
+        public string $unit = '',
+        public string $level = '',
+        public string $staircase = '',
+        public string $entrance = '',
+        public string $poBox = '',
+        public string $postcode = '',
+        public string $suburb = '',
+        public string $cityDistrict = '',
+        public string $city = '',
+        public string $island = '',
+        public string $stateDistrict = '',
+        public string $state = '',
+        public string $countryRegion = '',
+        public string $country = '',
+        public string $worldRegion = '',
     ) {}
+
+    public static function make(...$args): ParsedAddress
+    {
+        return new ParsedAddress(...$args);
+    }
 
     public static function fromLibpostalResponse(array $data): self
     {
@@ -47,7 +54,7 @@ class ParsedAddress
         $parameters = self::getParameters();
 
         $parameters
-            ->mapWithKeys(fn ($parameter) => [$parameter => ''])
+            ->mapWithKeys(fn (string $parameter) => [$parameter => ''])
             ->diffKeys($mappedData)
             ->each(fn (string $value, string $key) => $mappedData->put($key, $value));
 
@@ -56,5 +63,14 @@ class ParsedAddress
             ->toArray();
 
         return self::fromArray($mappedData);
+    }
+
+    public function toHttpResponse(): PromiseInterface
+    {
+        $data = self::getParameters()
+            ->map(fn (string $parameter) => [Str::lower($this->$parameter), Str::snake($parameter)])
+            ->toArray();
+
+        return Http::response($data);
     }
 }
--- tests/ParsedAddressTest.php
+++ tests/ParsedAddressTest.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Illuminate\Support\Facades\Http;
 use Innobrain\Libpostal\Dtos\ParsedAddress;
 
 it('can construct', function () {
@@ -20,4 +21,96 @@ it('can construct', function () {
         ->and($parsedAddress->city)->toBe('springfield')
         ->and($parsedAddress->state)->toBe('il')
         ->and($parsedAddress->postcode)->toBe('62701');
+});
+
+it('can turn to response', function () {
+    Http::preventStrayRequests();
+    Http::fake([
+        '*' => ParsedAddress::make(country: 'US')->toHttpResponse(),
+    ]);
+
+    $response = Http::get('https://example.com');
+
+    expect($response->json())->toBe([
+        [
+            '',
+            'house',
+        ],
+        [
+            '',
+            'category',
+        ],
+        [
+            '',
+            'near',
+        ],
+        [
+            '',
+            'house_number',
+        ],
+        [
+            '',
+            'road',
+        ],
+        [
+            '',
+            'unit',
+        ],
+        [
+            '',
+            'level',
+        ],
+        [
+            '',
+            'staircase',
+        ],
+        [
+            '',
+            'entrance',
+        ],
+        [
+            '',
+            'po_box',
+        ],
+        [
+            '',
+            'postcode',
+        ],
+        [
+            '',
+            'suburb',
+        ],
+        [
+            '',
+            'city_district',
+        ],
+        [
+            '',
+            'city',
+        ],
+        [
+            '',
+            'island',
+        ],
+        [
+            '',
+            'state_district',
+        ],
+        [
+            '',
+            'state',
+        ],
+        [
+            '',
+            'country_region',
+        ],
+        [
+            'us',
+            'country',
+        ],
+        [
+            '',
+            'world_region',
+        ],
+    ]);
 });
